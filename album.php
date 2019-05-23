98e5f4c2950823b52edf681c040b2a3ac35c0562
mittalarpan <mittalarpan1@gmail.com>
2019-05-23 16:08:36 +0530

    Second commit

--- album.php
+++ album.php
@@ -5,12 +5,20 @@
 
 <body>
 <h2> <b> OPTIONS </b> </h2>
+<br>
+<a href = "album.php?insert_image">Insert New Image</a>
+<br>
 <a href = "album.php?add_album">Add Album</a> 
 <br>
 <a href = "album.php?view_album">View Album</a> 
 <br>
 
 <?php
+
+if(isset($_GET['insert_image'])){
+	include('insert_image.php');  
+}
+
 if(isset($_GET['add_album'])){
    
    include('add_album.php') ; 
